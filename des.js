c01161fff941273a914a5ab304e109ff63a53070
louis cai <1290103192@qq.com>
2018-09-25 21:29:44 +0800

    basicUI

---
+++ des.js
@@ -0,0 +1,5 @@
+var button_decode = document.getElementById('decode_start');
+var decode_content = document.getElementById('de_content');
+button_decode.onclick = function () {
+    decode_content.innerText = 'hello';
+}
